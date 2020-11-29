941af2e16e554a5ed7d9db7b216d181d84e764cf
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2020-11-29 04:54:16 -0500

    Updated: Beautified code.

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-    var result = 'App is running'
+    var result = "App is running";
     response.send(result);
 }).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+    console.log("Application is successfully running: server is listening on PORT ", app.get('port'));
 });
 
 mongoose.connect(process.env.mongoPass, {
