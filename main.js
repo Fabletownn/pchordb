2aed6a0554c7c6c7cad1dce288d7871a0666308d
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2020-11-29 06:08:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.get('/', function(request, response) {
     console.log("Application is successfully running: server is listening on PORT ", app.get('port'));
 });
 
-mongoose.connect(process.env.mongopass, {
+mongoose.connect(process.env.mongoPass, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
