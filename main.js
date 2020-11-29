2791d00265b7a1870b751a8e1d2170577892ae60
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2020-11-28 21:38:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ const client = new Discord.Client();
 const botconfig = require('./botconfig.json')
 const mongoose = require('mongoose');
 
-const port = process.env.PORT || 3000 ;
-server.listen(port,()=>{  // do not add localhost here if you are deploying it
-    console.log("server listening to port "+port);
-});
-
 mongoose.connect(botconfig.mongoPass, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
