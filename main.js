a67517d26735d0ad915ce72bcedd0209c218072b
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2020-11-30 00:05:48 -0500

    Deleting unnecessary files.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const botconfig = require('./botconfig.json')
 const mongoose = require('mongoose');
 
 const PRE = require('./models/prefix.js');
