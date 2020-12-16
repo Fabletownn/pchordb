68789bcdbba9c4570d31f850ad784768faf46f2f
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2020-12-16 16:53:50 -0500

    Update dogfact.js

--- commands/dogfact.js
+++ commands/dogfact.js
@@ -5,9 +5,9 @@ module.exports = {
     description: '[GENERAL] This will generate and provide a random dog fact. <[setPrefix]dogfact>',
     execute(message) {
         message.delete();
-        
+
         let moderatorR = message.guild.roles.cache.find(role => role.name === "Moderator");
-        if (!message.member.roles.cache.has(moderatorR.id) && message.channel.id !== '780027707622424607') return;
+        if (!message.member.roles.cache.has(moderatorR.id) && message.channel.id !== '615594300108963867') return;
 
         request.get('https://dog-api.kinduff.com/api/facts').end((err, res) => {
             if (!err && res.status === 200) {
