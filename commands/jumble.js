aa67e4ff6e15faf052db8f2ae21f8f7c30042113
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2020-12-19 14:41:35 -0500

    Implementing two new commands.

---
+++ commands/jumble.js
@@ -0,0 +1,78 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const Jumble = require("jumble-words");
+const jumble = new Jumble();
+
+const ms = require("parse-ms");
+var cooldownVariable;
+
+module.exports = {
+    name: 'jumble',
+    description: '[GENERAL] This will initiate a game of Jumble Words. <[setPrefix]jumble>',
+    async execute(message) {
+        message.delete();
+
+        const word = jumble.generate();
+        let timeout = 5000;
+
+        let moderatorR = message.guild.roles.cache.find(role => role.name === "Moderator");
+        if (!message.member.roles.cache.has(moderatorR.id) && message.channel.id !== '789937524763000832') return;
+
+        if (timeout - (Date.now() - cooldownVariable) > 0) return;
+        const hintList = [word[0].word.substring(0, 3), word[0].word.substring(0, 4), word[0].word.substring(0, 5)];
+        const hint = hintList[Math.floor(Math.random() * hintList.length)];
+
+        cooldownVariable = Date.now();
+        message.channel.send(`**[🕹️] ${message.author.username}**, let's play **Jumble**! Your word will be shown shortly. __Get ready!__`).then(msg => {
+            setTimeout(async function() {
+                const gameEmbed = new Discord.MessageEmbed()
+                    .setAuthor(`Jumble Words | ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                    .setDescription(`Your jumbled word is **${word[0].jumble}**.\n**HINT**: ||It starts with "${hint}".||`)
+                    .setColor(`eb4bc9`)
+                    .setFooter(`In case of any concerning words, contact ModMail.`)
+                    .setTimestamp()
+
+                await msg.edit(`**${message.author.username}**, you have 15 seconds to answer. Get guessin'!`, { embed: gameEmbed });
+
+                const filter = m => m.author.id === message.author.id;
+                message.channel.awaitMessages(filter, {
+                        max: 1,
+                        error: ["time"],
+                        time: 15000,
+                    })
+                    .then(collected => {
+                        const answer = collected.first();
+                        if (word[0].word.toLowerCase === 'jumble' && answer.content.includes('+jumble')) return;
+
+                        if (!answer.content.toLowerCase().includes(word[0].word.toLowerCase())) {
+                            const incorrectEmbed = new Discord.MessageEmbed()
+                            .setAuthor(`Jumble Words | ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                            .setDescription(`🛎️ **Incorrect.** The non-jumbled word was "${word[0].word}".`)
+                            .setColor(`ff0000`)
+                            .setFooter(`The game has ended.`)
+                            .setTimestamp()
+                        
+                        answer.delete();
+                        return msg.edit(`**${message.author.username}**, nice try. Run the command to play again.`, { embed: incorrectEmbed }).then(m => m.delete({ timeout: 5000 }));
+                        }
+                        const correctEmbed = new Discord.MessageEmbed()
+                            .setAuthor(`Jumble Words | ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                            .setDescription(`🛎️ **Correct!** The non-jumbled word was "${word[0].word}".`)
+                            .setColor(`00ff00`)
+                            .setFooter(`The game has ended.`)
+                            .setTimestamp()
+                        
+                        answer.delete();
+                        return msg.edit(`**${message.author.username}**, nice work! Run the command to play again.`, { embed: correctEmbed }).then(m => m.delete({ timeout: 5000 }));
+                    })
+                    .catch(() => {
+                        message.channel.send(`Tiiiiime's up! You couldn't answer in time: the correct answer was "**${word[0].word}**"! RIP.`).then(m => m.delete({
+                            timeout: 5000
+                        }));
+                        return msg.delete();
+                    });
+            }, 3000);
+        });
+    }
+}
---
+++ commands/userinfo.js
@@ -0,0 +1,187 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+module.exports = {
+    name: 'userinfo',
+    description: '[GENERAL] This will provide information about either you, or a mentioned user. <[setPrefix]userinfo (<@member>)>',
+    execute(message) {
+        message.delete();
+
+        var mentionedUser = message.mentions.users.first();
+
+        let moderatorR = message.guild.roles.cache.find(role => role.name === "Moderator");
+        let administratorR = message.guild.roles.cache.find(role => role.name === "Administrator");
+        let itfR = message.guild.roles.cache.find(role => role.name === "I Talk Fortnite");
+
+        let moderatorR = message.guild.roles.cache.find(role => role.name === "Moderator");
+        if (!message.member.roles.cache.has(moderatorR.id) && message.channel.id !== '789937524763000832') return;
+
+        if (!mentionedUser) {
+            if (message.member.roles.cache.has(itfR.id)) {
+                const ownerInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${message.author.tag}`)
+                    .addField(`Joined Server`, `📆 ${message.member.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${message.member.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${message.author.createdAt.toUTCString().substr(0, 16)}\n🕛 ${message.author.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Owner`)
+                    .addField(`Roles`, `${message.member.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${message.author.id}`)
+                    .setColor(message.member.displayColor)
+                    .setThumbnail(message.author.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${message.author.tag}**:`, {
+                    embed: ownerInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            } else
+            if (message.member.roles.cache.has(administratorR.id)) {
+                const administratorInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${message.author.tag}`)
+                    .addField(`Joined Server`, `📆 ${message.member.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${message.member.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${message.author.createdAt.toUTCString().substr(0, 16)}\n🕛 ${message.author.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Administrator`)
+                    .addField(`Roles`, `${message.member.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${message.author.id}`)
+                    .setColor(message.member.displayColor)
+                    .setThumbnail(message.author.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${message.author.tag}**:`, {
+                    embed: administratorInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            } else
+            if (message.member.roles.cache.has(moderatorR.id)) {
+                const moderatorInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${message.author.tag}`)
+                    .addField(`Joined Server`, `📆 ${message.member.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${message.member.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${message.author.createdAt.toUTCString().substr(0, 16)}\n🕛 ${message.author.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Moderator`)
+                    .addField(`Roles`, `${message.member.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${message.author.id}`)
+                    .setColor(message.member.displayColor)
+                    .setThumbnail(message.author.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${message.author.tag}**:`, {
+                    embed: moderatorInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            } else {
+                const memberInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${message.author.tag}`)
+                    .addField(`Joined Server`, `📆 ${message.member.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${message.member.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${message.author.createdAt.toUTCString().substr(0, 16)}\n🕛 ${message.author.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Member`, true)
+                    .addField(`Roles`, `${message.member.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${message.author.id}`)
+                    .setColor(message.member.displayColor)
+                    .setThumbnail(message.author.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${message.author.tag}**:`, {
+                    embed: memberInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            }
+        } else if (mentionedUser) {
+            var mentionedMember = message.guild.member(mentionedUser);
+
+            if (mentionedMember.roles.cache.has(itfR.id)) {
+                const ownerInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${mentionedUser.tag}`)
+                    .addField(`Joined Server`, `📆 ${mentionedMember.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedMember.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${mentionedUser.createdAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedUser.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Owner`)
+                    .addField(`Roles`, `${mentionedMember.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${mentionedUser.id}`)
+                    .setColor(mentionedMember.displayColor)
+                    .setThumbnail(mentionedUser.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${mentionedUser.tag}**:`, {
+                    embed: ownerInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            } else
+            if (mentionedMember.roles.cache.has(administratorR.id)) {
+                const administratorInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${mentionedUser.tag}`)
+                    .addField(`Joined Server`, `📆 ${mentionedMember.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedMember.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${mentionedUser.createdAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedUser.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Administrator`)
+                    .addField(`Roles`, `${mentionedMember.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${mentionedUser.id}`)
+                    .setColor(mentionedMember.displayColor)
+                    .setThumbnail(mentionedUser.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${mentionedUser.tag}**:`, {
+                    embed: administratorInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            } else
+            if (mentionedMember.roles.cache.has(moderatorR.id)) {
+                const moderatorInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${mentionedUser.tag}`)
+                    .addField(`Joined Server`, `📆 ${mentionedMember.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedMember.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${mentionedUser.createdAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedUser.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Moderator`)
+                    .addField(`Roles`, `${mentionedMember.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${mentionedUser.id}`)
+                    .setColor(mentionedMember.displayColor)
+                    .setThumbnail(mentionedUser.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${mentionedUser.tag}**:`, {
+                    embed: moderatorInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            } else {
+                const memberInfoEmbed = new Discord.MessageEmbed()
+                    .setTitle(`User Information | ${mentionedUser.tag}`)
+                    .addField(`Joined Server`, `📆 ${mentionedMember.joinedAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedMember.joinedAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`Joined Discord`, `📆 ${mentionedUser.createdAt.toUTCString().substr(0, 16)}\n🕛 ${mentionedUser.createdAt.toUTCString().substr(16, 50)}`, true)
+                    .addField(`User Status`, `Server Member`)
+                    .addField(`Roles`, `${mentionedMember.roles.cache.map(roleList => `${roleList}`).join(' ')}`)
+                    .setFooter(`User ID: ${mentionedUser.id}`)
+                    .setColor(mentionedMember.displayColor)
+                    .setThumbnail(mentionedUser.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+
+                message.channel.send(`Displaying user information for **${mentionedUser.tag}**:`, {
+                    embed: memberInfoEmbed
+                }).then(m => m.delete({
+                    timeout: 30000
+                }));
+            }
+        } else {
+            message.channel.send(`**[❌] ${message.author.username}**, an error occurred trying to fetch User Data.`).then(m => m.delete({
+                timeout: 5000
+            }));
+        }
+    }
+}
