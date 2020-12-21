40961ab052aeadcaa523b5242ad6cfb5bce6e9a8
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2020-12-20 20:35:02 -0500

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -4,11 +4,11 @@ const client = new Discord.Client();
 module.exports = {
     name: 'vote',
     description: '[MODERATION] This will add Power Chord upvote and downvote emotes to the message you specified the ID for. <[setPrefix] vote <message ID>>',
-    execute(message, args) {
+    execute(message) {
         message.delete();
         
         let moderatorR = message.guild.roles.cache.find(role => role.name === "Moderator");
-        if (!message.member.roles.cache.has(moderatorR.id)) return;
+        if (!message.member.roles.cache.has(moderatorR.id) && message.channel.id !== '789937524763000832') return;
 
         var messageID = message.content.split("vote ");
 
@@ -25,8 +25,9 @@ module.exports = {
         }
 
         message.channel.messages.fetch(messageID[1]).then(m => {
-            m.react(message.guild.emojis.cache.get("778318625328332810")).then(m.react(message.guild.emojis.cache.get("778318624552779776")));
-        }).catch(e => {
+            m.react(`<:zzITFUpvote:778318625328332810>`).then(m.react(`<:zzITFDownvote:778318624552779776>`));
+        }).catch(error => {
+            console.log(error);
             return message.channel.send(`**[📰] ${message.author.username}**, please ensure that message is within this channel.`).then(m => m.delete({
                 timeout: 5000
             }));
