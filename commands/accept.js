0ba43d382e3b5ea21399d138a55e8b977ac4e770
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2021-01-01 03:55:05 -0500

    Updating commands!

--- commands/accept.js
+++ commands/accept.js
@@ -1,10 +1,11 @@
-const Discord = require("discord.js")
-const client = new Discord.Client();
+const Discord = require("discord.js");
 
 module.exports = {
     name: 'accept',
     description: '[MODERATION] This command is restricted to the Appeals guild & channel(s) only. It will accept a ban appeal. <[setPrefix]accept <@user>>',
-    execute(message, args) {
+    execute(message) {
+        const client = message.client;
+
         if (message.guild.id !== '685876599199236173') return;
 
         let moderatorR = message.guild.roles.cache.find(role => role.name === "Moderator");
@@ -28,6 +29,21 @@ module.exports = {
 
             acceptedMember.send(`**${appealAcceptionMention.username}**, your Ban Appeal has been accepted, and you have been unbanned from the **I Talk Server**.\nMake sure to read the rules upon re-joining to prevent further punishments.\n\nPermanent Invite Link: https://discord.gg/italkfortnite`).then(() => {
                 acceptedMember.kick(`Appeal was accepted by ${message.author.tag}.`).then(() => {
+                    const acceptEmbed = new Discord.MessageEmbed()
+                        .setAuthor(`Appeal Accepted | ${message.author.tag}`, message.author.displayAvatarURL({
+                            dynamic: true
+                        }))
+                        .addField(`User`, appealAcceptionMention.tag, true)
+                        .addField(`Time Accepted`, `${new Date().toLocaleTimeString()} UTC\n${new Date()}`)
+                        .setThumbnail(appealAcceptionMention.displayAvatarURL({
+                            dynamic: true
+                        }))
+                        .setColor(`6dff48`)
+
+                    client.channels.cache.get('794486722356183052').send({
+                        embed: acceptEmbed
+                    });
+
                     message.channel.send(`**[📜] ${message.author.username}**, successfully accepted **${appealAcceptionMention.tag}**'s appeal!`).then(m => m.delete({
                         timeout: 10000
                     }));
--- commands/deny.js
+++ commands/deny.js
@@ -1,10 +1,11 @@
-const Discord = require("discord.js")
-const client = new Discord.Client();
+const Discord = require("discord.js");
 
 module.exports = {
     name: 'deny',
     description: '[MODERATION] This command is restricted to the Appeals guild & channel(s) only. It will deny a ban appeal. <[setPrefix]deny <@user>>',
-    execute(message, args) {
+    execute(message) {
+        const client = message.client;
+
         if (message.guild.id !== '685876599199236173') return;
 
         let moderatorR = message.guild.roles.cache.find(role => role.name === "Moderator");
@@ -35,9 +36,25 @@ module.exports = {
             }));
 
             deniedMember.send(`**${appealDenialMention.username}**, your Ban Appeal has been **rejected**. You have been banned from both the **I Talk Server** and the **Appeals Server**.\n\nAs a result, you can no longer appeal or be unbanned from the **I Talk Server**.`);
+            
             deniedMember.ban({
                 reason: `Appeal was denied by ${message.author.tag}.`
             }).then(() => {
+                const denyEmbed = new Discord.MessageEmbed()
+                    .setAuthor(`Appeal Denied | ${message.author.tag}`, message.author.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .addField(`User`, appealDenialMention.tag, true)
+                    .addField(`Time Denied`, `${new Date().toLocaleTimeString()} UTC\n${new Date()}`)
+                    .setThumbnail(appealDenialMention.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setColor(`ff0000`)
+
+                client.channels.cache.get('794486722356183052').send({
+                    embed: denyEmbed
+                });
+
                 message.channel.send(`**[📜] ${message.author.username}**, successfully denied the appeal. Proper action has been taken.`).then(m => m.delete({
                     timeout: 10000
                 }));
