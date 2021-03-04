041f7e0590179361a1862817cf80d8c813d96827
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2021-03-04 01:44:10 -0500

    Updating commands.

--- commands/eventclose.js
+++ commands/eventclose.js
@@ -8,37 +8,38 @@ module.exports = {
         if (!message.member.roles.cache.has("614196214078111745") && !message.member.roles.cache.has("685878871748378644") && !message.member.roles.cache.has("797145089297350736") && !message.member.roles.cache.has("614195872347062273")) return;
 
         message.delete({ timeout: 5000 });
-
-        /*discord streams: 816705412451008542
-        server events: 731212776936308777
-        fort customs: 731217324765479053*/
         
         const serverEvents = message.guild.channels.cache.get("815790709646163968");
         const eventsChat = message.guild.channels.cache.get("720002083226386553");
+        const codesChan = message.guild.channels.cache.get("771394751316099133");
 
         serverEvents.updateOverwrite('816705412451008542', {
             VIEW_CHANNEL: false,
-        }, `Closing up shop. | ${message.author.tag} [1/6].`);
+        }, `Closing up shop. | ${message.author.tag} [1/7].`);
 
         eventsChat.updateOverwrite('816705412451008542', {
             VIEW_CHANNEL: false,
-        }, `Closing up shop. | ${message.author.tag} [2/6].`);
+        }, `Closing up shop. | ${message.author.tag} [2/7].`);
 
         serverEvents.updateOverwrite('731212776936308777', {
             VIEW_CHANNEL: false,
-        }, `Closing up shop. | ${message.author.tag} [3/6].`);
+        }, `Closing up shop. | ${message.author.tag} [3/7].`);
 
         eventsChat.updateOverwrite('731212776936308777', {
             VIEW_CHANNEL: false,
-        }, `Closing up shop. | ${message.author.tag} [4/6].`);
+        }, `Closing up shop. | ${message.author.tag} [4/7].`);
 
         serverEvents.updateOverwrite('731217324765479053', {
             VIEW_CHANNEL: false,
-        }, `Closing up shop. | ${message.author.tag} [5/6].`);
+        }, `Closing up shop. | ${message.author.tag} [5/7].`);
 
         eventsChat.updateOverwrite('731217324765479053', {
             VIEW_CHANNEL: false,
-        }, `Closing up shop. | ${message.author.tag} [6/6].`);
+        }, `Closing up shop. | ${message.author.tag} [6/7].`);
+
+        codesChan.updateOverwrite('731217324765479053', {
+            VIEW_CHANNEL: false,
+        }, `Closing up shop. | ${message.author.tag} [7/7].`);
 
         message.react("✅");
         
