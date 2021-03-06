00749a1e4ce5cec1f21685c1f472e76cf4d64c4d
Fabletownn <72483934+Fabletownn@users.noreply.github.com>
2021-03-06 04:00:19 -0500

    Adding additional feature to command.

--- commands/spotlight.js
+++ commands/spotlight.js
@@ -12,13 +12,12 @@ module.exports = {
         var attachment1;
         var attachment2;
         var attachment3;
+        var fleetCard;
 
         const artArray = ["More work!", "More!", "Even more!", "More of their work!"]
         const artResult = artArray[Math.floor(Math.random() * artArray.length)];
 
-        if (!message.member.roles.cache.has("614195872347062273")) return;
-
-        message.channel.send(`**[🎭] ${message.author.username}**, who will be the Spotlight for this week? Please ensure you're either mentioning them (<@ID>).`).then(msg => {
+        message.channel.send(`**[🎭] ${message.author.username}**, who will be the Spotlight for this week? Please ensure you're mentioning them (e.g. ${message.author}).`).then(msg => {
             const userQuestion = m => m.author.id === message.author.id && m.mentions.users.first();
             message.channel.awaitMessages(userQuestion, {
                 max: 1,
@@ -84,44 +83,59 @@ module.exports = {
                                     });
                                 }
 
-                                msg.delete();
-                                message.channel.send(`**[🎭] ${message.author.username}**, please confirm the details below by saying "yes" or "no." Please understand URLs will not be provided.\nSpotlight: <@${userID}>\nDescription & Socials: ${userDesc}\nArtwork #1 (URL): ${attachment1}\nArtwork #2 (URL): ${attachment2}\nArtwork #3 (URL): ${attachment3}`);
+                                msg.edit(`**[🎭] ${message.author.username}**, please provide the **Fleet Card** image to be announced.`)
 
-                                const confirmQuestion = m => m.author.id === message.author.id && (m.content.toLowerCase().startsWith("yes") || m.content.toLowerCase().startsWith("no"));
-                                message.channel.awaitMessages(confirmQuestion, {
+                                const fleetQuestion = m => m.author.id === message.author.id && m.attachments.size !== 0
+                                message.channel.awaitMessages(fleetQuestion, {
                                     max: 1,
                                     time: 999999,
                                     errors: ['time']
                                 }).then(collected => {
-                                    collected.first().delete();
+                                    if (collected.first().attachments.size > 0) {
+                                        collected.first().attachments.forEach(fleetAtt => {
+                                            fleetCard = fleetAtt.url;
+                                        });
+                                    }
 
-                                    if (collected.first().content.toLowerCase().startsWith("yes")) {
-                                        client.channels.cache.get('772175886031650827').send(`✨ Creator for this Week: <@${userID}>\n\n${userDesc}`, {
-                                            files: [attachment1]
-                                        }).then(() => {
-                                            client.channels.cache.get('772175886031650827').send(`${artResult}`, {
-                                                files: [attachment2]
+                                    msg.delete();
+                                    message.channel.send(`**[🎭] ${message.author.username}**, please confirm the details below by saying "yes" or "no." Please understand URLs will not be provided.\nSpotlight: <@${userID}>\nDescription & Socials: ${userDesc}\n\nArtwork #1 (URL): ${attachment1}\nArtwork #2 (URL): ${attachment2}\nArtwork #3 (URL): ${attachment3}\nFleet Card (URL): ${fleetCard}`);
+    
+                                    const confirmQuestion = m => m.author.id === message.author.id && (m.content.toLowerCase().startsWith("yes") || m.content.toLowerCase().startsWith("no"));
+                                    message.channel.awaitMessages(confirmQuestion, {
+                                        max: 1,
+                                        time: 999999,
+                                        errors: ['time']
+                                    }).then(collected => {
+                                        collected.first().delete();
+    
+                                        if (collected.first().content.toLowerCase().startsWith("yes")) {
+                                            client.channels.cache.get('803711612548284426').send(`✨ Creator for this Week: <@${userID}>\n\n${userDesc}`, {
+                                                files: [attachment1]
                                             }).then(() => {
-                                                client.channels.cache.get('772175886031650827').send(`${artResult}`, {
-                                                    files: [attachment3]
+                                                client.channels.cache.get('803711612548284426').send(`${artResult}`, {
+                                                    files: [attachment2]
+                                                }).then(() => {
+                                                    client.channels.cache.get('803711612548284426').send(`${artResult}`, {
+                                                        files: [attachment3]
+                                                    });
                                                 });
                                             });
-                                        });
-
-                                        client.channels.cache.get('614193406842765375').send(`- Our new Server Spotlight <@${userID}>!\nCheck out <#772175886031650827> for more information! <:zITFHype:680882437894701083>`).then(toPin => {
-                                            toPin.pin();
-                                        });
-
-                                        client.channels.cache.get('614500763997175824').send(`- Our new Server Spotlight <@${userID}>!\nCheck out <#772175886031650827> for more information! <:zITFHype:680882437894701083>`);
-
-                                        var toMember = message.guild.members.cache.get(userID);
-                                        toMember.roles.add('772176660958281737');
-
-                                        message.channel.send(`Sent in all channels that apply!`);
-                                    } else {
-                                        return message.channel.send(`Cancelled. Feel free to readjust and make any changes.`);
-                                    }
-                                })
+    
+                                            client.channels.cache.get('614500763997175824').send(`- Our new Server Spotlight, <@${userID}>!\nCheck out <#772175886031650827> for more information! <:zITFHype:680882437894701083>`, { files: [fleetCard]}).then(toPin => {
+                                                toPin.pin();
+                                            });
+    
+                                            client.channels.cache.get('614193406842765375').send(`- Our new Server Spotlight, <@${userID}>!\nCheck out <#772175886031650827> for more information! <:zITFHype:680882437894701083>`, { files: [fleetCard]});
+    
+                                            var toMember = message.guild.members.cache.get(userID);
+                                            toMember.roles.add('772176660958281737');
+    
+                                            message.channel.send(`**[🎭] ${message.author.username}**, the Server Spotlight, **${client.users.cache.get(userID).username}**, has been announced!\n\nyay amazing great excellent exceptional favorable great marvelous positive satisfactory satisfying superb valuable wonderful ace boss bully capital choice crack nice pleasing prime rad sound spanking sterling super superior welcome worthy admirable agreeable commendable congenial deluxe first-class first-rate gnarly gratifying honorable neat precious recherché reputable select shipshape splendid stupendous super-eminent super-excellent tip-top up to snuff sheesh`).then(m => m.delete({ timeout: 15000 }));
+                                        } else {
+                                            return message.channel.send(`Cancelled. Feel free to readjust and make any changes.`);
+                                        }
+                                    });
+                                });
                             });
                         });
                     });
